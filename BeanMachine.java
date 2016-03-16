cbf2efa0f5978bb872776d4f357a56e0585e4a2a
U10416008 <cp4n3gj9448@gmail.com>
2016-03-17 02:34:20 +0800

    Version3
    add beans

--- BeanMachine.java
+++ BeanMachine.java
@@ -62,10 +62,44 @@ class DisplayBeanMachine extends Pane {
 		line[i] = new Line(centerX-60 + i*20,centerY+110,centerX-60 + i*20,centerY+150);
 		
 	}
+	for(int i = 0; i <= 27 ; i++){
+		bean[i] = new Circle();
+		bean[i].setFill(Color.GREEN);
+		bean[i].setRadius(5);
+	}
+    bean[0].setCenterX(centerX);
+	bean[0].setCenterY(centerY-62);
+	for(int i = 0; i <= 1 ; i++){
+		bean[i+1].setCenterX(centerX-10 + i*20);
+		bean[i+1].setCenterY(centerY-34);
+	}
+	for(int i = 0; i <= 2 ; i++){
+		bean[i+3].setCenterX(centerX-20 + i*20);
+		bean[i+3].setCenterY(centerY-6);
+	}
+	for(int i = 0; i <= 3 ; i++){
+		bean[i+6].setCenterX(centerX-30 + i*20);
+		bean[i+6].setCenterY(centerY+22);
+	}
+	for(int i = 0; i <= 4 ; i++){
+		bean[i+10].setCenterX(centerX-40 + i*20);
+		bean[i+10].setCenterY(centerY+50);
+	}
+	for(int i = 0; i <= 5 ; i++){
+		bean[i+15].setCenterX(centerX-50 + i*20);
+		bean[i+15].setCenterY(centerY+78);
+	}
+	for(int i = 0; i <= 6 ; i++){
+		bean[i+21].setCenterX(centerX-60 + i*20);
+		bean[i+21].setCenterY(centerY+105);
+	}
     getChildren().clear();
     getChildren().add(polyline); 
 	for(int i = 0;i <= 6; i++){
 		getChildren().add(line[i]);    
+	}
+	for(int i = 0; i <= 27 ; i++){
+		getChildren().add(bean[i]);
 	}
 	
   }
